beb0918119d67f74b8ebfe4083003faad9e9b9bc
Paul Abernathy <paul.abernathy7@gmail.com>
2019-08-15 21:42:18 -0400

    find and findbyid work

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,10 +1,20 @@
 const db = require("../data/db-config");
 
 module.exports = {
-  //   find,
-  //   findById,
+  find,
+  findById
   //   findSteps,
   //   add,
   //   update,
   //   remove
 };
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
