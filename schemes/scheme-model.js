9002ae8258b560289bd9663d0b236affc8383d75
Paul Abernathy <paul.abernathy7@gmail.com>
2019-08-15 22:29:05 -0400

    everything works but find steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,11 +2,11 @@ const db = require("../data/db-config");
 
 module.exports = {
   find,
-  findById
-  //   findSteps,
-  //   add,
-  //   update,
-  //   remove
+  findById,
+  //   findSteps
+  add,
+  update,
+  remove
 };
 
 function find() {
@@ -17,4 +17,30 @@ function findById(id) {
   return db("schemes")
     .where({ id })
     .first();
+}
+
+// function findSteps(id){
+
+//     return db('steps')
+//     .join('schemes','steps.scheme_id','schemes.id')
+//     .where('steps.scheme_id' , '=' , id)
+// }
+
+//must destructure array ID
+
+async function add(scheme) {
+  const [id] = await db("schemes").insert(scheme);
+  return findById(id);
+}
+
+async function update(changes, id) {
+  await db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
 }
